c6fe8f22af3869f8a13f9d7d003625fb30af6cc0
dell <valentina.osetrov@gmail.com>
2024-04-13 22:06:33 +0200

    rajout variables

--- projet/static/index.js
+++ projet/static/index.js
@@ -12,27 +12,50 @@ $(document).ready(function(){
         window.location.href = "song_list.html";
     });
     $("#speaknow").click(function(){
-        alert("speak now clicked!");
+        var album = "speaknow";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
     });
     $("#red").click(function(){
+        var album = "red";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("red clicked!");
     });
     $("#1989").click(function(){
+        var album = "1989";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("1989 clicked!");
     });
     $("#reputation").click(function(){
+        var album = "reputation";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("reputation clicked!");
     });
     $("#lover").click(function(){
+        var album = "lover";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("lover clicked!");
     });
     $("#folklore").click(function(){
+        var album = "folklore";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("flkl clicked!");
     });
     $("#evermore").click(function(){
+        var album = "evermore";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("ev clicked!");
     });
     $("#midnights").click(function(){
+        var album = "midnights";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
         alert("m clicked!");
     });
 
