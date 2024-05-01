4450df9ecf4b6798c420a1c43c85984817a13d62
Sandra Jagodzińska <sandrajagodzinska@mbp-sandra.home>
2024-05-01 15:26:09 +0200

    suppresion des alerts

--- projet/static/index.js
+++ projet/static/index.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     // When the image with id "speaknow" is clicked, show an alert
     $("#debut").click(function(){
         var album = "debut";
-        alert(album);
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
     });
@@ -20,44 +19,35 @@ $(document).ready(function(){
         var album = "red";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("red clicked!");
     });
     $("#1989").click(function(){
         var album = "1989";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("1989 clicked!");
     });
     $("#reputation").click(function(){
         var album = "reputation";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("reputation clicked!");
     });
     $("#lover").click(function(){
         var album = "lover";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("lover clicked!");
     });
     $("#folklore").click(function(){
         var album = "folklore";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("flkl clicked!");
     });
     $("#evermore").click(function(){
         var album = "evermore";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("ev clicked!");
     });
     $("#midnights").click(function(){
         var album = "midnights";
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
-        alert("m clicked!");
     });
-
-
 });
