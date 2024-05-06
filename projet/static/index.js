63f42d510ea49d9af92deb63fc1863ae8aa6f4f3
Sandra Jagodzińska <sandrajagodzinska@mbp-sandra.home>
2024-05-06 17:58:41 +0200

    petit changements

--- projet/static/index.js
+++ projet/static/index.js
@@ -56,4 +56,60 @@ $(document).ready(function(){
         localStorage.setItem("album", album);
         window.location.href = "song_list.html";
     });
+    $("#debut2").click(function(){
+        var album = "debut";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#fearless2").click(function(){
+        var album = "fearless";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#speaknow2").click(function(){
+        var album = "speaknow";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#red2").click(function(){
+        var album = "red";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#19892").click(function(){
+        var album = "1989";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#reputation2").click(function(){
+        var album = "reputation";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#lover2").click(function(){
+        var album = "lover";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#folklore2").click(function(){
+        var album = "folklore";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#evermore2").click(function(){
+        var album = "evermore";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#midnights2").click(function(){
+        var album = "midnights";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    $("#unknown2").click(function(){
+        var album = "unknown";
+        localStorage.setItem("album", album);
+        window.location.href = "song_list.html";
+    });
+    alert(album);
 });
